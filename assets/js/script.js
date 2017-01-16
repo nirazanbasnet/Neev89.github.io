c578413101ac48e523c0a5588c810f6ce8158d93
Nirajan <nirajanb89@gmail.com>
2017-01-16 13:09:17 +0545

    Codepen work section implementation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,41 @@
+//This is where we apply opacity to the arrow
+$(window).scroll( function(){
+
+    //get scroll position
+    var topWindow = $(window).scrollTop();
+    //multiply by 1.5 so the arrow will become transparent half-way up the page
+    var topWindow = topWindow * 1.5;
+
+    //get height of window
+    var windowHeight = $(window).height();
+
+    //set position as percentage of how far the user has scrolled
+    var position = topWindow / windowHeight;
+    //invert the percentage
+    position = 1 - position;
+
+    //define arrow opacity as based on how far up the page the user has scrolled
+    //no scrolling = 1, half-way up the page = 0
+    $('.arrow-wrap').css('opacity', position);
+
+});
+
 $(function() {
+
+    //From css-tricks for smooth scrolling:
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
     /*========================================
      =            Animated Letters            =
      ========================================*/
@@ -73,6 +110,7 @@ $(function() {
     initHeadline();
 
     /*-----Dropable & Draggable-----*/
+
     $( ".social-link" ).draggable({
         cursor: "crosshair",
         revert: "valid",
@@ -82,6 +120,34 @@ $(function() {
         hoverClass: "social-drag-hover",
         drop: function( event, ui ) {
             window.open(ui.draggable.attr("href"));
+        }
+    });
+
+    /* IsometricGrid */
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    new IsoGrid(document.querySelector('.isolayer--deco1'), {
+        transform : 'translateX(33vw) translateY(-340px) rotateX(45deg) rotateZ(45deg)',
+        stackItemsAnimation : {
+            properties : function(pos) {
+                return {
+                    translateZ: (pos+1) * 30,
+                    rotateZ: getRandomInt(-4, 4)
+                };
+            },
+            options : function(pos, itemstotal) {
+                return {
+                    type: dynamics.bezier,
+                    duration: 500,
+                    points: [{"x":0,"y":0,"cp":[{"x":0.2,"y":1}]},{"x":1,"y":1,"cp":[{"x":0.3,"y":1}]}],
+                    delay: (itemstotal-pos-1)*40
+                };
+            }
+        },
+        onGridLoaded : function() {
+            classie.add(document.body, 'grid-loaded');
         }
     });
 });
